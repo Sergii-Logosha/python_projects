f9856c9463acbfab1fa8cb4d65026e872e747d53
Sergii Logosha <sergiilogosha@gmail.com>
2022-07-03 16:50:43 +0300

    Project pizza_order.py is created

---
+++ beginner_level/pizza_order.py
@@ -0,0 +1,30 @@
+# Sergii Logosha, 03 Jul 2022
+
+# The program helps User to calculate the bill for pizza order.
+
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+
+price = 0
+if size == "S":
+    price = 15
+    if add_pepperoni == "Y":
+        price += 2
+    if extra_cheese == "Y":
+        price += 1
+elif size == "M":
+    price = 20
+    if add_pepperoni == "Y":
+        price += 3
+    if extra_cheese == "Y":
+        price += 1
+elif size == "L":
+    price = 25
+    if add_pepperoni == "Y":
+        price += 3
+    if extra_cheese == "Y":
+        price += 1
+print(f"Your final bill is: ${price}.")
+
