8aa534c0bafe62732a5778c9ca16540f3a645056
Sergii-Logosha <serhiilohosha@gmail.com>
2022-09-26 20:48:50 +0300

    Project computer_time.py is created

---
+++ general/computer_time/computer_time.py
@@ -0,0 +1,9 @@
+# Sergii Logosha, 26.09.22.
+
+# The program take time from the computer and displays it on the screen
+
+from time import asctime
+
+time = asctime()
+print(f"The current time is: {time}")
+
