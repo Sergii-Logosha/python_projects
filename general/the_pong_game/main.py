05d3102c40ba47d42bbf097cf85b77fb02682a52
Sergii-Logosha <serhiilohosha@gmail.com>
2023-03-06 21:20:02 +0200

    Project The Pong Game is started

---
+++ general/the_pong_game/main.py
@@ -0,0 +1,11 @@
+# 06.03.2023 Sergii Logosha sergiilogosha@gmail.com
+
+from turtle import Screen
+
+screen = Screen()
+
+screen.setup(width=800, height=600)
+screen.title('The Pong Game')
+screen.bgcolor('green')
+
+screen.exitonclick()
