b21c7c705c167fa6d4dccf6166f083428a3a567b
Sergii-Logosha <serhiilohosha@gmail.com>
2023-03-12 17:29:41 +0200

    paddle object is added to the game

--- general/the_pong_game/main.py
+++ general/the_pong_game/main.py
@@ -1,11 +1,39 @@
 # 06.03.2023 Sergii Logosha sergiilogosha@gmail.com
+# Last modified 12.03.2023
 
-from turtle import Screen
+from turtle import Screen, Turtle
 
 screen = Screen()
+paddle = Turtle()
 
 screen.setup(width=800, height=600)
 screen.title('The Pong Game')
 screen.bgcolor('green')
+screen.tracer(0)
+
+paddle.penup()
+paddle.color('white')
+paddle.shape('square')
+paddle.shapesize(stretch_len=1, stretch_wid=5)
+paddle.goto(350, 0)
+
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+screen.listen()
+screen.onkey(go_up, 'Up')
+screen.onkey(go_down, 'Down')
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
