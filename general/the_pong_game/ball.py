e240015a6399305453468d68f1e8d51609070ba6
Sergii-Logosha <serhiilohosha@gmail.com>
2023-03-19 20:27:11 +0200

    Ball class is created, functionality of ball bouncing added

---
+++ general/the_pong_game/ball.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape('circle')
+        self.color('yellow')
+        self.penup()
+        self.x_move = 10
+        self.y_move = 10
+
+    def move(self):
+        new_x_cor = self.xcor() + self.x_move
+        new_y_cor = self.ycor() + self.y_move
+        self.goto(new_x_cor, new_y_cor)
+
+    def bounce(self):
+        self.y_move *= -1
+
--- general/the_pong_game/main.py
+++ general/the_pong_game/main.py
@@ -1,12 +1,15 @@
 # 06.03.2023 Sergii Logosha sergiilogosha@gmail.com
-# Last modified 14.03.2023
+# Last modified 19.03.2023
 
+from ball import Ball
+from time import sleep
 from turtle import Screen
 from paddle import Paddle
 
 screen = Screen()
 r_paddle = Paddle(350, 0)
 l_paddle = Paddle(-350, 0)
+ball = Ball()
 
 screen.setup(width=800, height=600)
 screen.title('The Pong Game')
@@ -22,6 +25,15 @@ screen.onkey(l_paddle.go_down, 's')
 
 game_is_on = True
 while game_is_on:
+    sleep(0.1)
     screen.update()
+    ball.move()
+
+    # Ball collides with the top wall
+    if ball.ycor() > 290:
+        ball.bounce()
+
+    if ball.xcor() < -390 or ball.xcor() > 390:
+        game_is_on = False
 
 screen.exitonclick()
